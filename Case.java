8d9cc456e7a3169b709d8cf467be85fdbb1d7747
keerthivasan0229 <keerthivasan0229@gmail.com>
2025-09-16 15:24:43 +0530

    javacaseprogram

---
+++ Case.java
@@ -0,0 +1,8 @@
+public class Case {
+    public static void main(String[] args){
+        String txt ="keerthivasan";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+    }
+    
+}
