22cb7a752436d27aa4aa3ae28ad2f0ba0f9019e7
keerthivasan0229 <keerthivasan0229@gmail.com>
2025-09-21 12:41:09 +0530

    Dt

--- palindrome.java
+++ palindrome.java
@@ -1,11 +1,12 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String str = "madam";
+        String str = "1222"; // you can change this word
         String rev = new StringBuilder(str).reverse().toString();
+
         if (str.equals(rev)) {
-            System.out.println("Palindrome");
+            System.out.println("Palindrome: " + str);
         } else {
-            System.out.println("Not Palindrome");
+            System.out.println("Not Palindrome: " + str);
         }
     }
 }
